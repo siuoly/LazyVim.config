ffa5b2515c64a63f4a87fb05553009c434ace1de
x260-siuoly <louis@gmial.com>
2024-04-20 23:11:25 +0800

    lazy.vim 10 days check update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -28,7 +28,7 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = { enabled = true, frequency = 86400 * 10 }, -- automatically check for plugin updates, 10 days
   performance = {
     rtp = {
       -- disable some rtp plugins
