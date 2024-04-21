22357b18e52909af81638cfdbaf2d529e13073c2
x260-siuoly <louis@gmial.com>
2024-04-21 14:01:17 +0800

    python

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,101 +1,22 @@
+vim.cmd([[ 
+let b:slime_bracketed_paste = 1 
+let b:slime_cell_delimiter = "##%" 
+]])
 local keymap = vim.keymap.set
-local opts = { silent = true, buffer = true }
+local opts = { silent = true, buffer = true, noremap = true }
+-- vim-slime config -----
+-- space-cr:region
 
-local function map_exit(exit_key)
-  keymap("n", exit_key, "<Cmd>bd<cr>", opts)
-end
-local function toggle_winsize()
-  if vim.api.nvim_win_get_height(0) < 15 then
-    print("maxize")
-    vim.api.nvim_win_set_height(0, 100) -- maxize window
-  else
-    print("small")
-    vim.api.nvim_win_set_height(0, 10) -- small window window
-  end
-end
-local function run_python_file()
-  vim.cmd([[w|new|res 10]])
-  vim.cmd("terminal python3 #:p")
-  map_exit("q")
-  map_exit("<f5>")
-  keymap("n", "<f6>", toggle_winsize, opts)
-end
-local function copy_breakpoint_fileline()
-  local breakpoint = vim.fn.expand("%") .. ":" .. vim.fn.line(".")
-  vim.fn.setreg("+", breakpoint)
-end
-keymap("n", "yb", copy_breakpoint_fileline, { buffer = true })
-keymap("n", "<f5>", run_python_file, { buffer = true })
-------------- Ipython Setting --------------------------------------------
-local ipython = {}
-
-ipython.run = function()
-  vim.cmd([[ 
-  call feedkeys("\<c-w>\<c-w>\<c-\>\<c-n>",'n')  
-  call feedkeys("\"\"pi\<cr>",'n')
-  call feedkeys("\<c-\>\<c-n>\<c-w>\<c-p>",'n')  
-  ]])
-  -- go to terminal window" paste register" , key enter go back"
-end
-ipython.copy_run = function(copy)
-  vim.api.nvim_command("normal m'")
-  vim.api.nvim_command("normal " .. copy)
-  vim.api.nvim_command("normal `'")
-  ipython.run()
-end
-ipython.send = function(cmd)
-  local sendword = vim.fn.expandcmd(cmd)
-  vim.fn.setreg("", sendword)
-  ipython.run()
-end
--- add newline for ipython block code new line
-local reg_add_newline = function()
-  vim.fn.setreg("", vim.fn.getreg("") .. "\n")
-end
-ipython.run_class = function()
-  ipython.copy_run("yac")
-end
-ipython.run_function = function()
-  ipython.copy_run("yaf")
-end
-ipython.run_block = function()
-  ipython.copy_run("yip")
-  reg_add_newline()
-end
-ipython.run_line = function()
-  ipython.copy_run("yy")
-end
-ipython.run_select = function()
-  ipython.copy_run("y")
-  reg_add_newline()
-end
-ipython.run_file = function()
-  ipython.send("\\%run %")
-end
-ipython.run_word = function()
-  ipython.send("<cword>")
-end
-ipython.run_WORD = function()
-  ipython.send("<cWORD>")
-end
-keymap("n", "<space>r", ipython.run_file, {})
-keymap("n", "<space>c", ipython.run_class, {})
-keymap("n", "<space>f", ipython.run_function, {})
-keymap("n", "<space><space>", ipython.run_block, {})
-keymap("x", "<space><space>", ipython.run_select, {})
-keymap("n", "<space>q", ipython.run_line, {})
-keymap("n", "<space>w", ipython.run_word, {})
-keymap("n", "<space>W", ipython.run_WORD, {})
-
-vim.api.nvim_create_user_command("Ipy", "TermExec cmd='ipython' direction=vertical", {})
-ipython.clear = function()
-  ipython.send("clear")
-end
-ipython.exit = function()
-  ipython.send("exit")
-end
-
-------------- debugger setting -------------------------------------------
+keymap("n", "<space>r"      , [[<Cmd>SlimeSend0 expandcmd('\\%run %').. "\n"<cr>]], { desc="Slime run python file",silent = true, buffer = true })
+keymap("n", "<space>w"      , [[<Cmd>SlimeSend0 expandcmd('<cword>').. "\n"<cr>]] , { desc="Slime run python cursor word",silent = true, buffer = true })
+keymap("n", "<space>W"      , [[<Cmd>SlimeSend0 expandcmd('<cWORD>').. "\n"<cr>]] , { desc="Slime run python cursor WORD",silent = true, buffer = true })
+keymap("n", "<space>n"      , [[<Cmd>SlimeSendCurrentLine<cr>]]                   , { desc="Slime run python current line",silent = true, buffer = true })
+keymap("n", "<space><space>", [[<Plug>SlimeParagraphSend]]                        , { desc="Slime run python paragraph",silent = true, buffer = true })
+keymap("x", "<space><space>", "<Plug>SlimeRegionSend"                             , { desc="Slime run python select region",noremap = true, buffer = true })
+keymap("n", "<c-cr>"        , [[<Plug>SlimeSendCell]]                             , { desc="Slime run python notation region",remap = true, buffer = true })
+-- keymap("n", "<space>c", ipython.run_class, {opts})
+-- keymap("n", "<space>f", ipython.run_function, {opts})
+------------ pdb -----------------------
 local pdb = {}
 pdb.file_point = function()
   local line = vim.api.nvim_win_get_cursor(0)[1]
