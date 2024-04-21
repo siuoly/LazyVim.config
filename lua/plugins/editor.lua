9fa457e724ae4b9313b492e5d563b86f3fa3e40e
x260-siuoly <louis@gmial.com>
2024-04-21 14:02:02 +0800

    vim-slime

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -113,4 +113,36 @@ return {
       })
     end,
   },
+  {
+    "jpalardy/vim-slime",
+    ft = "python",
+    config = function()
+      vim.cmd([[
+        let g:slime_config_defaults = get(g:,"slime_config_defaults",{})
+        let g:slime_config_defaults["python_ipython"] = 0
+        let g:slime_config_defaults["dispatch_ipython_pause"] = 100
+
+        function! _EscapeText_python(text)
+          if slime#config#resolve("python_ipython") && len(split(a:text,"\n")) > 1
+            return ["%cpaste -q\n", slime#config#resolve("dispatch_ipython_pause"), a:text, "--\n"]
+          else
+            let empty_lines_pat = '\(^\|\n\)\zs\(\s*\n\+\)\+'
+            let no_empty_lines = substitute(a:text, empty_lines_pat, "", "g")
+            let dedent_pat = '\(^\|\n\)\zs'.matchstr(no_empty_lines, '^\s*')
+            let dedented_lines = substitute(no_empty_lines, dedent_pat, "", "g")
+            let except_pat = '\(elif\|else\|except\|finally\)\@!'
+            let add_eol_pat = '\n\s[^\n]\+\n\zs\ze\('.except_pat.'\S\|$\)'
+            return substitute(dedented_lines, add_eol_pat, "\n", "g")
+          end
+        endfunction
+      ]])
+      vim.g.slime_target = "tmux"
+      -- vim.g.slime_default_config = {"socket_name" = "default", "target_pane" = "{last}"}
+      vim.g.slime_default_config = {
+        -- Lua doesn't have a string split function!
+        socket_name = vim.api.nvim_eval('get(split($TMUX, ","), 0)'),
+        target_pane = "{top-right}",
+      }
+    end,
+  },
 }
