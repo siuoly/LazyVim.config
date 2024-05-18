9f3b39d62391d999444a64326f46ac1444fdb388
x260-siuoly <louis@gmial.com>
2024-05-18 14:09:44 +0800

    markdown unuser plugin, using default feature to fold

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,4 @@ vim.o.pumblend = 0 -- transparent, 0 is opaque
 vim.o.winend = 30 -- transparent, 0 is opaque
 vim.o.clipboard = "unnamedplus"
 vim.api.nvim_set_hl(0,'@markup.italic.markdown_inline',{link="markdownItalic"})
+vim.g.markdown_folding=1
