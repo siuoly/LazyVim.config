82e95cb000f599ee66659e65fc9e990a27c1c097
x260-siuoly <louis@gmial.com>
2024-05-18 14:11:09 +0800

    markdown italic highlight

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,7 +3,7 @@
 -- Add any additional options here
 vim.o.rnu = false
 vim.o.pumblend = 0 -- transparent, 0 is opaque
-vim.o.winend = 30 -- transparent, 0 is opaque
+-- vim.o.winend = 30 -- transparent, 0 is opaque
 vim.o.clipboard = "unnamedplus"
-vim.api.nvim_set_hl(0,'@markup.italic.markdown_inline',{link="markdownItalic"})
+vim.api.nvim_set_hl(0,'@markup.italic.markdown_inline',{link="Italic"})
 vim.g.markdown_folding=1
