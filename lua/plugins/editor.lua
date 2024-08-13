107e0eb730fd26de957eb4b6ee861fadade789c5
x260-siuoly <louis@gmial.com>
2024-08-13 15:45:01 +0800

    dial fix

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -5,6 +5,7 @@ return {
       local augend = require("dial.augend")
       opts.groups.markdown = {
         augend.misc.alias.markdown_header,
+        augend.integer.alias.decimal,
         augend.date.alias["%Y-%m-%d"],
         augend.date.alias["%H:%M"],
       }
@@ -60,18 +61,13 @@ return {
     keys = {
       { "<m-t>", "<cmd>AerialToggle<cr>", desc = "Code outline.[T]oc " },
     },
-    ft = { "markdown" },
+    -- ft = { "markdown" },
     opts = {
       on_attach = function(bufnr)
         -- Jump forwards/backwards with '{' and '}'
         vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
         vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
       end,
-    },
-    -- Optional dependencies
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-tree/nvim-web-devicons",
     },
   },
   {
