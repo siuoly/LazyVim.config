5eb268c691e8030e31682f529f12698e88c86944
x260-siuoly <louis@gmial.com>
2024-08-16 15:45:20 +0800

    neo-tree

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,91 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    keys = {
+      {
+        "<c-n>",
+        function()
+          if vim.o.ft == "neo-tree" then
+            require("neo-tree.command").execute({ action = "close" })
+          else
+            require("neo-tree.command").execute({ focus = true, dir = LazyVim.root() })
+          end
+        end,
+        desc = "Explorer NeoTree (Root Dir)",
+        remap = true,
+      },
+    },
+    opts = function(_, opts)
+      -- opts.filesystem.follow_current_file= {
+      --   enabled = false,       -- automatically exapnd directory
+      --   leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+      -- }
+      opts.filesystem.bind_to_cwd = true
+      opts.filesystem.commands = {
+        trash = function(state)
+          local path = state.tree:get_node().path
+          vim.fn.system({ "trash", vim.fn.fnameescape(path) })
+          require("neo-tree.sources.manager").refresh(state.name)
+        end,
+      }
+      opts.window.mappings = vim.tbl_extend("force", opts.window.mappings, {
+        ["<c-s>"] = "open_split",
+        ["<c-v>"] = "open_vsplit",
+        ["s"] = "noop",
+        ["S"] = "noop",
+      })
+
+      local fc = require("neo-tree.sources.filesystem.commands")
+      local fi = require("neo-tree.sources.filesystem")
+      opts.filesystem.window = {
+        mappings = {
+          ["d"] = "noop",
+          ["dd"] = "trash",
+          ["/"] = "noop",
+          ["."] = "noop",
+          ["<c-]>"] = "set_root",
+          ["h"] = {
+            function(state)
+              local node = state.tree:get_node()
+              if node.type == "directory" and node:is_expanded() then
+                fi.toggle_directory(state, node)
+              else
+                require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+              end
+            end,
+            desc = "go parent or close directory",
+          },
+          ["l"] = {
+            function(state)
+              local node = state.tree:get_node()
+              if node.type == "directory" then
+                if not node:is_expanded() then
+                  fi.toggle_directory(state, node)
+                elseif node:has_children() then
+                  require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
+                end
+              else
+                fc.open(state)
+              end
+            end,
+            desc = "open or toggle directory",
+          },
+          ["<cr>"] = {
+            function(state)
+              fc.open(state)
+              require("neo-tree.sources.common.commands").close_window(state)
+            end,
+            desc = "open and close tree",
+          },
+        },
+      }
+      opts.enable_diagnostics = false
+      opts.default_component_configs.git_status = {
+        symbols = {
+          unstaged = "",
+          staged = "",
+        },
+      }
+    end,
+  },
+}
