9792408c0e863ad2f404e7bcc8a933870343ef14
x260-siuoly <louis@gmial.com>
2024-08-24 00:02:37 +0800

    cursor

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,5 @@ vim.o.rnu = false
 vim.o.pumblend = 0 -- transparent, 0 is opaque
 -- vim.o.winend = 30 -- transparent, 0 is opaque
 vim.o.clipboard = "unnamedplus"
+vim.o.confirm = false
+vim.o.gcr="n-v-c-sm:block-Cursor,i-ci-ve:Cursor2,r-cr-o:hor20"
